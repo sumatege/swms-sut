6b05125cc7503e14c11551bc8dc3074f92219c5d
Sumate Gesornchuen <46530532+sumatege@users.noreply.github.com>
2021-08-14 22:59:40 +0700

    Update config-main.php

--- config-main.php
+++ config-main.php
@@ -1,17 +1,17 @@
 <?php
 
-
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "swms_database";
+*/
 
-/*
 $servername = "localhost";
 $username = "id17235519_swms2021";
 $password = "[MSc-EOe<1hV[-\c";
 $dbname = "id17235519_swms_database	";
-*/
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
