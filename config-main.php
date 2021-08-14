c29a14e28582ba840d9b397828b42316be405c70
Sumate Gesornchuen <46530532+sumatege@users.noreply.github.com>
2021-08-15 00:13:53 +0700

    Add files via upload

--- config-main.php
+++ config-main.php
@@ -7,15 +7,18 @@ $password = "";
 $dbname = "swms_database";
 */
 
-$servername = "http://157.245.149.179";
+$servername = "localhost";
 $username = "io";
 $password = "7777777777yU";
-$dbname = "swms_database	";
+$dbname = "swms_database";
 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+?>
